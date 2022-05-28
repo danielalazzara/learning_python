15e281fead26c9e64954523045c531d1ec7c4d76
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-05-28 22:27:07 +0100

    Delete encode_the_labels_as_categorical_variables.py
    wrong repository

--- encode_the_labels_as_categorical_variables.py
+++
@@ -1,8 +0,0 @@
-# Define the lambda function: categorize_label
-categorize_label = lambda x: x.astype('category')
-
-# Convert df[LABELS] to a categorical type
-df[LABELS] = df[LABELS].apply(categorize_label, axis=0)
-
-# Print the converted dtypes
-print(df[LABELS].dtypes)
